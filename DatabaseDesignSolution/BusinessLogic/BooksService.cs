9b478d909a1d08876c02c5b97b9ce2453058a55d
mirainegalea <56063951+mirainegalea@users.noreply.github.com>
2020-12-04 10:41:04 +0100

    Repository Commit

---
+++ DatabaseDesignSolution/BusinessLogic/BooksService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using Common;
+
+namespace BusinessLogic
+{
+    public class BooksService
+    {
+        private BookRepository _booksRepo;
+
+        public BooksService()
+        {
+            _booksRepo = new BookRepository();
+        }
+
+        public void Add(Book b)
+        {
+            _booksRepo.Add(b);
+        }
+
+        public List<Book> GetBooks() //using List because the presentation is going to get a definite/final list
+        {
+            return _booksRepo.GetBooks().ToList();
+        }
+
+        public List<Book> GetBooks(int genreId)
+        {
+            // you can use either this 
+
+            //return _booksRepo.GetBooks().Where(book => book.GenreFK == genreId).ToList();
+
+            //or this
+
+            var list = from book in _booksRepo.GetBooks()
+                       where book.GenreFK == genreId
+                       select book;
+            return list.ToList();
+        }
+    }
+}
---
+++ DatabaseDesignSolution/DataAccess/BookRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace DataAccess
+{
+    public class BookRepository : ConnectionClass
+    {
+        public BookRepository():base()
+        {}
+
+        //Add a book
+        
+        public void Add(Book b)
+        {
+            MyConnection.Books.Add(b);
+            MyConnection.SaveChanges();
+        }
+
+        public IQueryable<Book> GetBooks()
+        {
+            return MyConnection.Books;
+        }
+    }
+}
